040baae465fdb71dd21703371e31c439f15d7200
shayanmalinda <shayanmalinda@gmail.com>
2019-03-07 23:11:59 +0530

    Login, Home, Profile,  Sigup UI Completed

---
+++ app/src/main/java/com/shayanmalinda/quickcabs/ProfileActivity.java
@@ -0,0 +1,25 @@
+package com.shayanmalinda.quickcabs;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+    public void logout(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
