6dca5d9f9dbe98bfbd49bc5ded082dabe11cd3f1
shayanmalinda <shayanmalinda@gmail.com>
2019-04-12 01:07:50 +0530

    Register new user saving to database completed

---
+++ app/src/main/java/com/shayanmalinda/quickcabs/LoginBackgroundTask.java
@@ -0,0 +1,97 @@
+package com.shayanmalinda.quickcabs;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class LoginBackgroundTask extends AsyncTask<String,Void,String> {
+
+    Context ctx;
+    LoginBackgroundTask(Context ctx)
+    {
+        this.ctx=ctx;
+    }
+
+
+    protected String doInBackground(String... params) {
+
+        String reg_url="http://idexserver.tk/b05/shayan/quickcabs/login/add.php";
+        String method=params[0];
+        String text = "";
+        if(method.equals("register"))
+        {
+            String username=params[1];
+            String email=params[2];
+            String phone=params[3];
+            String pass=params[4];
+            try {
+                URL url=new URL(reg_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream os=httpURLConnection.getOutputStream();
+
+                String data= URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"+
+                        URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+
+                        URLEncoder.encode("phone","UTF-8")+"="+URLEncoder.encode(phone,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(pass,"UTF-8");
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                int statusCode = httpURLConnection.getResponseCode();
+                if (statusCode == 200) {
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null)
+                        sb.append(line).append("\n");
+
+                    text = sb.toString();
+                    bufferedWriter.close();
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return text;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(ctx,result,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
--- app/src/main/java/com/shayanmalinda/quickcabs/RegisterActivity.java
+++ app/src/main/java/com/shayanmalinda/quickcabs/RegisterActivity.java
@@ -1,14 +1,92 @@
 package com.shayanmalinda.quickcabs;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username,email,phone,pass1,pass2;
+    String strUsername, strEmail, strPhone, strPass1, strPass2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setTitle(R.string.signup2);
+
+        username = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        pass1 = (EditText) findViewById(R.id.pass1);
+        pass2 = (EditText) findViewById(R.id.pass2);
     }
+
+    public void signup(View v) {
+
+        strUsername = username.getText().toString();
+        strEmail = email.getText().toString();
+        strPhone = phone.getText().toString();
+        strPass1 = pass1.getText().toString();
+        strPass2 = pass2.getText().toString();
+//        String ServerURL = "http://idexserver.tk/b05/shayan/quickcabs/login/add.php";
+
+        if(strUsername.isEmpty()){
+            Toast.makeText(this, "Username is Empty", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(strEmail.isEmpty()){
+            Toast.makeText(this, "Email is Empty", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(strPhone.isEmpty()){
+            Toast.makeText(this, "Phone Number is Empty", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(strPass1.isEmpty() && strPass2.isEmpty()){
+            Toast.makeText(this, "Password is Empty", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            if(strPass1.equals(strPass2)){
+
+                String method="register";
+                LoginBackgroundTask backgroundTask=new LoginBackgroundTask(this);
+                backgroundTask.execute(method,strUsername,strEmail,strPhone,strPass1);
+
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+
+            }
+
+            else{
+                Toast.makeText(this, "Password Mismatched", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+    }
+
+
+
 }
+
