84f30e3484cef8566584fbcd137de2f1a68b745d
shayanmalinda <shayanmalinda@gmail.com>
2019-04-18 21:30:59 +0530

    MultDex Error Fixed

--- app/src/main/java/com/shayanmalinda/quickcabs/MainActivity.java
+++ app/src/main/java/com/shayanmalinda/quickcabs/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+//import android.support.multidex.MultiDex;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        MultiDex.install(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
